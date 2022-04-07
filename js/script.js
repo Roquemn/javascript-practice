2bb98eec1e155d0956ae78cdd3778096f1bfbd4b
Roque Martinez <roquemn5@gmail.com>
2022-04-07 15:41:39 -0700

    added console name, styled html

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// This is a single line comment //
+
+
+
+/* This is a multi-line comment */
+
+
+var myName = {
+    firstname: "Roque" , lastname:"Martinez", favoritefood: "Tamales"
+}
+
+console.table(myName);
