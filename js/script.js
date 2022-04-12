2fdbaefafbedb55d26aeae342db858f9ab53507d
Roque Martinez <roquemn5@gmail.com>
2022-04-12 12:12:33 -0700

    logged length, BigInt, Numbers, aand used typeof samples

--- js/script.js
+++ js/script.js
@@ -18,3 +18,32 @@ console.table(myName);
 // console.log("First Name: Roque")
 
 
+
+// Length
+let string = "Supercalifragilisticexpialidocious"
+console.log(string.length)
+
+//To determine the type
+console.log(typeof string) 
+
+// Number
+let age = 35
+console.log(typeof age)
+
+// Bigint
+let hugeString = 123451234512345123451234512345n
+console.log(typeof hugeString)
+
+// Boolean
+let isLightOn = true
+console.log(typeof isLightOn)
+
+
+
+
+
+
+
+
+
+
